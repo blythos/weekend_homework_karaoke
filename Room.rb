19c3941cafccaf9230dbaac1574b05ab738a420c
Stephen Blythe <stephenblythe@gmail.com>
2019-07-26 13:24:25 +0100

    Adds adding guest functionality to Room

--- Room.rb
+++ Room.rb
@@ -7,4 +7,12 @@ class Room
     @guests = []
   end
 
+  def guests_amount()
+    return @guests.length
+  end
+
+  def add_guest(guest)
+    @guests << guest
+  end 
+
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -2,14 +2,21 @@ require("minitest/autorun")
 require("minitest/rg")
 
 require_relative("../Room")
+require_relative("../Guest")
 
 class TestRoom < MiniTest::Test
 
   def setup()
     @room = Room.new("Aztec Suite")
+    @guest1 = Guest.new("Kyle")
   end
 
   def test_get_room_name()
     assert_equal("Aztec Suite", @room.name)
+  end
+
+  def test_add_guest()
+    @room.add_guest(@guest1)
+    assert_equal(1, @room.guests_amount)
   end
 end
